7212458d7d004301e335854c1abbba70a93ff40b
Yate Hong <yate@memeboxglobal.com>
2016-11-10 17:48:49 +0800

    submit

---
+++ src/assets/js/src/default.js
@@ -0,0 +1,7 @@
+/**
+ * Created by yate on 16/9/14.
+ */
+$(function(){
+   var b = 0;
+   console.info(b);
+});
---
+++ src/assets/js/src/test.js
@@ -0,0 +1,7 @@
+/**
+ * Created by yate on 16/9/14.
+ */
+function test(){
+    var i = 0;
+    return i;
+}

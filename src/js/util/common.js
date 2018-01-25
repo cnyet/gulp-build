2d708e6156f36a68e570925870f689fb32986418
yate <yateup@163.com>
2018-01-25 18:24:36 +0800

    add outdatebrowser

--- src/js/util/common.js
+++ src/js/util/common.js
@@ -3,21 +3,32 @@ require.config({
     baseUrl: "js/lib",     //根目录
     paths:{
       "jquery": "jquery.min",
-      "bootstrap": "bootstrap.min"
+      "bootstrap": "bootstrap.min",
+      "outdatedbrowser": "outdatedbrowser.min",  
     },
     shim: {
-      bootstrap: {
+      "bootstrap": {
         deps: ['jquery']
+      },
+      "outdatedbrowser": {
+        exports: "outdatedBrowser"
       },
     }
 });
 //定义模块
-define(['jquery', "bootstrap"], function($){
+define(['jquery', "bootstrap", "outdatedbrowser"], function($){
+  //检测是否是现代浏览器
+  outdatedBrowser({
+    lowerThan: 'transform',
+    languagePath: '' 
+  });
+
   //判断浏览器对media query的支持
   if(!Modernizr.mq('only all')) {
     $('<script src="/js/lib/respond.min.js"></script>').appendTo("head");
     console.log("Your browser can't support the media query feature");      
   }
+
   var add = function(x, y){
       console.log("这是common");
       return x+y;

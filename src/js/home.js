49de5bc0cd294df25b653f94eb97500adf0409a0
yate <yateup@163.com>
2018-02-09 17:48:07 +0800

    add layui

--- src/js/home.js
+++ src/js/home.js
@@ -1,11 +1,25 @@
 //对模块的加载行为进行自定义
 require.config({
   paths:{
-    "common": "util/common"
+    "common": "util/common",
+    "layui": "layui"
+  },
+  shim: {
+    "layui": {
+      deps: ["common"]
+    } 
   }
 });
 
-require(["common"], function(){
+require(["common", "layui"], function(){
+  //调用layui模块
+  layui.use(['element', "layer", 'laydate', 'form'], function(){
+    layui.laydate.render({
+      base: "",
+      elem: '.publishDate', //指定元素
+      theme: '#c80505'
+    })    
+  }); 
   console.log("这是home");
 });
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,10 @@
 //对模块的加载行为进行自定义
 require.config({
   paths:{
-    "test": "util/test"
-  }
+    "test": "util/test",
+    
+  },
+
 });
 
 require(["test"], function(test){

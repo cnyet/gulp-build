bba2ae6a79c8e1b8eb285be85be20c7575d02a2b
yate <yateup@163.com>
2018-03-13 13:00:05 +0800

    add filter ployfill

--- src/js/util/common.js
+++ src/js/util/common.js
@@ -34,5 +34,31 @@ define(['jquery', "bootstrap", "outdatedbrowser"], function($){
     $('<script src="/js/lib/respond.min.js"></script>').appendTo("head");
     console.log("Your browser can't support the media query feature");      
   }
+
+  //filter方法支持旧版本浏览器
+  if (!Array.prototype.filter) {
+    Array.prototype.filter = function(fun) {
+      if (this === void 0 || this === null)
+        throw new TypeError();
+
+      var t = Object(this);
+      var len = t.length >>> 0;
+      if (typeof fun !== "function")
+        throw new TypeError();
+
+      var res = [];
+      var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+      for (var i = 0; i < len; i++)
+      {
+        if (i in t)
+        {
+          var val = t[i];
+          if (fun.call(thisArg, val, i, t))
+            res.push(val);
+        }
+      }
+      return res;
+    };
+  }
   
 });
